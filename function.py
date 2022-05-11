2cff7485305b9c1e883dc460aaf3da53d65df397
Rokit12 <robinsonchege95@gmail.com>
2022-05-11 08:31:51 +0300

    Third commit

---
+++ function.py
@@ -0,0 +1,27 @@
+#Creating a function
+# def function(apple):
+# 	print(apple)
+# 	return;
+# function("This is a first created function")
+
+#defining a function
+# def items(list):
+# 	list.append([2, 4, 7, 8])
+# 	print("Values are inside the function", list)
+# 	return
+# list = [73, 67, 89]
+# items(list) #function call
+
+#Default argument
+# def function(name):
+# 	print(name)
+# 	return
+# function("Antony")
+
+
+#Importing a file
+import math
+
+a = 5
+b = 10
+print(a + b)
